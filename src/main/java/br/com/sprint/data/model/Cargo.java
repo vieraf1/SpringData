3b9bc6a288b7aecd62ab58f6a62d6dbcc216a130
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-05-30 22:09:17 -0300

    Configurando projeto e criando entidade Cargo
    Configurando projeto e criando entidade Cargo

---
+++ src/main/java/br/com/sprint/data/model/Cargo.java
@@ -0,0 +1,35 @@
+package br.com.sprint.data.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CARGOS")
+public class Cargo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String descricao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
